37930d55c483619aef63e55d4926694a35ffbed9
priyankraychura <priyankraychura@gmail.com>
2025-10-16 13:11:10 +0530

    cores changed

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const {
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}));
+app.use(cors());
 
 // In-memory storage
 const users = new Map(); // username -> user object
